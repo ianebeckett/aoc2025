49eb56fc3cf03f33441f492ee60fb174bbf59ca6
Ian Beckett <ianedwardbeckett@gmail.com>
2025-12-05 07:08:37 -0600

    upsolved 3

--- 3.cpp
+++ 3.cpp
@@ -1,72 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-part 1
-int jolts(){
+//part 1
 
-    int sum{};
-    string input;
-    while (getline(cin, input)) {
-        if (input.empty()) break;
-
-        char tens = '0';
-        for (int i = 0; i < input.size() - 1; ++i) {
-            if (input[i] > tens) {
-                tens = input[i];
-            }
-        }
-
-        int pos = input.find_first_of(tens);
-        string suffix = input.substr(pos+1);
-
-        char ones = '0';
-        for (int i = 0; i < suffix.size(); ++i) {
-            if (suffix[i] > ones) {
-                ones = suffix[i];
-            }
-        }
-
-        int tensVal = tens - '0';
-        int onesVal = ones - '0';
-
-        sum += 10 * tensVal + onesVal;
-
-    }
-
-    return sum;
-}
-
-//part 2 failed, doesn't terminate
-// long long jolts(){
+// int jolts() {
 //
-//     long long sum{};
+//     int sum{};
 //     string input;
 //     while (getline(cin, input)) {
 //         if (input.empty()) break;
 //
-//         string found{};
-//         int need = 12;
-//         int nFound = 0;
-//         while (nFound < need) {
-//
-//             char digit = '0';
-//             int j = 0;
-//             for (int i = 0; i < input.size() - need + nFound + 1; ++i) {
-//                 if (input[i] > digit) {
-//                     digit = input[i];
-//                     j = i;
-//                 }
-//                 i = j;
+//         char tens = '0';
+//         for (int i = 0; i < input.size() - 1; ++i) {
+//             if (input[i] > tens) {
+//                 tens = input[i];
 //             }
-//
-//             found.push_back(digit);
-//             ++nFound;
 //         }
-//         sum += stoll(found);
+//
+//         int pos = input.find_first_of(tens);
+//         string suffix = input.substr(pos+1);
+//
+//         char ones = '0';
+//         for (int i = 0; i < suffix.size(); ++i) {
+//             if (suffix[i] > ones) {
+//                 ones = suffix[i];
+//             }
+//         }
+//
+//         int tensVal = tens - '0';
+//         int onesVal = ones - '0';
+//
+//         sum += 10 * tensVal + onesVal;
+//
 //     }
 //
 //     return sum;
 // }
+
+pair<char, int> greatest(string s) {
+    char greatest = '0';
+    int idx = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] > greatest) {
+            greatest = s[i];
+            idx = i;
+        }
+    }
+    return {greatest, idx};
+}
+
+//part 2
+long long jolts() {
+    long long sum{};
+    string input;
+    while (getline(cin, input)) {
+        if (input.empty()) break;
+        string joltage{};
+        int need = 12;
+        int nFound = 0;
+        int i = 0;
+        while (nFound < need) {
+            int save = need - nFound;
+            int j = input.size() - save + 1; // upper bound pos
+            auto chosen = greatest(input.substr(i, j-i));
+            joltage.push_back(chosen.first);
+            i = i + chosen.second + 1;
+            ++nFound;
+        }
+        //cerr << "joltage: " << joltage << endl;
+        sum += stoll(joltage);
+    }
+    return sum;
+}
 
 int main() {
 
